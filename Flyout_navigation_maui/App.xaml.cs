ca6f99abe8370ac8da9547452500862c12725f0d
Daniel <juandanielolivasmartinez@gmail.com>
2024-09-23 14:54:12 -0600

    Agregar archivos de proyecto.

---
+++ Flyout_navigation_maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Flyout_navigation_maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
